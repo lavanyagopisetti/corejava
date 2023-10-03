1619e257ced73ed3249394b04f3df68fc69f7508
SvkpSkill1 <SvkpSkill1@10.0.0.103>
2023-10-03 14:10:55 +0530

    first commit

---
+++ corejava1/src/programs/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob=new Encapsulation();//created object
+		ob.serialno=33;//accessing the dtamember by using dot operotor
+		ob.name="lavanya";
+		ob.age=21;
+		ob.show();
+
+	}
+
+}
