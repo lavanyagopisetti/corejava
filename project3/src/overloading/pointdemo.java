d2a7386cf094b3a00eb598b11b43dfc87be0b167
SvkpSkill1 <SvkpSkill1@10.0.0.132>
2023-10-05 14:03:25 +0530

    first commit

---
+++ project3/src/overloading/pointdemo.java
@@ -0,0 +1,10 @@
+package overloading;
+
+public class pointdemo {
+	
+public static void main(String[] args) {
+//ToDo Auto-generate method stub	
+	Point p=new Point();
+	System.out.println(p);
+}
+}
