9dda3e9964c7dceb44bb3db34dc51659b0377d7a
SvkpSkill1 <SvkpSkill1@192.168.1.57>
2023-10-28 15:33:19 +0530

    commit

---
+++ corejava1/src/com/tns/collection/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package com.tns.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     ArrayList list1=new ArrayList();
+     System.out.println("the size of list"+list1.isEmpty());
+	}
+
+}
